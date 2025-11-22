84c06d6157ef928d7ba08a459aabde5c320f96e1
SongJi-eun6 <ssjieun32@gmail.com>
2025-11-22 12:43:06 +0900

    충돌 해결

--- src/pages/column/column.jsx
+++ src/pages/column/column.jsx
@@ -1,11 +1,9 @@
 
 import React from "react";
-import Header from "../../components/atoms/Header/header";
 
 export default function Column() {
   return (
     <div className="app-wrapper">
-      <Header />
       <h1>칼럼페이지</h1>
     </div>
   );
